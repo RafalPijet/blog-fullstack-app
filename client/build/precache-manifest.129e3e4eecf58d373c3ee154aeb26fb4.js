dfa19d97609cffb061b4864575960d1706270f10
Rafal Pijet <stronglopez@wp.pl>
2019-09-16 22:42:19 +0200

    Next and next

--- client/build/precache-manifest.129e3e4eecf58d373c3ee154aeb26fb4.js
+++ client/build/precache-manifest.129e3e4eecf58d373c3ee154aeb26fb4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d316ad4e52a4486cb008cba811e0fd4e",
+    "revision": "b412ac072d0dfa6c84a9bac07572c187",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.a710ddf5.chunk.css"
   },
   {
-    "revision": "8cd495bbe37de97a2291",
+    "revision": "09bcf1e362b49d75a7d5",
     "url": "/static/css/main.5723664f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.40691937.chunk.js"
   },
   {
-    "revision": "8cd495bbe37de97a2291",
-    "url": "/static/js/main.03de4703.chunk.js"
+    "revision": "09bcf1e362b49d75a7d5",
+    "url": "/static/js/main.bf1d7f1c.chunk.js"
   },
   {
     "revision": "83e7f8b54e61b1cbddf8",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02bce86e871140c90afb8abc679281b9.js"
+  "/precache-manifest.129e3e4eecf58d373c3ee154aeb26fb4.js"
 );
 
 self.addEventListener('message', (event) => {
