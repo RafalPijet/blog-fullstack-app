1b6c511e347504197f682d930c5098db8da2d37e
Rafal Pijet <stronglopez@wp.pl>
2019-09-24 20:03:27 +0200

    Fix problem on heroku deploy

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    PORT: 8000,
+    PORT: process.env.PORT || 8000,
     DB: 'mongodb+srv://LopezPL:TechnoParty2204@cluster0-cpzdn.gcp.mongodb.net/test?retryWrites=true&w=majority'
 };
