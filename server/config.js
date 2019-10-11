a8c757454d7278171d16327316fd0563e47cb29f
Rafal Pijet <stronglopez@wp.pl>
2019-10-11 10:27:42 +0200

    Change data access to db

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
     PORT: process.env.PORT || 8000,
-    DB: 'mongodb+srv://LopezPL:TechnoParty2204@cluster0-cpzdn.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    DB: process.env.DB || 'mongodb://localhost:27017/mernapp'
 };
